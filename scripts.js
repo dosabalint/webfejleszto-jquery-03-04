376a16101ecff6185b3b9ed07b3966b64b811159
dosabalint <dosa.balint@gmail.com>
2018-04-28 23:54:49 +0200

    initial commit

---
+++ scripts.js
@@ -0,0 +1,62 @@
+$(function() {
+  // elemek összegyűjtése
+  let $button = $('button');
+
+  // változók
+  let gallery;
+  
+  // galéria inicializálás
+  InitGallery();
+
+  // gombnyomásra reagálás
+  $button.click(function() {
+    // galéria megnyitása
+    gallery.init();
+  });
+
+  //////////////////////////////////////////////////
+
+  // galéria inicialitálás, képek megadásas
+  function InitGallery() {
+    let pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    let items = [
+      {
+        src: 'https://placekitten.com/600/400',
+        w: 600,
+        h: 400
+      },
+      {
+        src: 'https://placekitten.com/1200/901',
+        w: 1200,
+        h: 901
+      },
+      {
+        src: 'https://placekitten.com/1200/700',
+        w: 1200,
+        h: 700
+      },
+      {
+        src: 'https://placekitten.com/600/700',
+        w: 600,
+        h: 700
+      }
+    ];
+
+    // define options (if needed)
+    let options = {
+      // optionName: 'option value'
+      // for example:
+      index: 0 // start at first slide
+    };
+
+    // Initializes and opens PhotoSwipe
+    gallery = new PhotoSwipe(
+      pswpElement,
+      PhotoSwipeUI_Default,
+      items,
+      options
+    );
+  }
+});
